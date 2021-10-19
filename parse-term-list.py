c6046a2bc689776838d85dce7f6f7a07a31a7d89
Dominic Betts <dobett@microsoft.com>
2021-10-19 15:46:42 +0100

    Update output formats

--- parse-term-list.py
+++ parse-term-list.py
@@ -3,8 +3,7 @@ import sys
 import regex as re
 from slugify import slugify
 
-def getKey(item):
-    return item['term']
+services = {'iot-hub':'Iot Hub','iot-central':'IoT Central','iot-fundamentals':'IoT fundamentals','iot-dps':'Device Provisioning Service','iot-edge':'IoT Edge','digital-twins':'Digital Twins','iot-develop':'Device developer'}
 
 class Outputter:
     def __init__(self, fileName, outputFormat):
@@ -12,10 +11,14 @@ class Outputter:
         self.outputFormat = outputFormat
         with open(fileName) as f:
             data = yaml.safe_load(f)
-            filteredTerms = filter(lambda term: term[outputFormat], data['terms'])
+            if outputFormat in ['customer-facing', 'contributor-guide']:
+                filteredTerms = filter(lambda term: term[outputFormat], data['terms'])
+            else: #dump-all
+                filteredTerms = data['terms']
             self.terms = sorted(filteredTerms, key=lambda t: t['term'])
 
             # Get a list of term names in descending size order so we match the longest terms first
+            # This list is used for cross-linking terms in termlist
             self.termNames = [t['term'] for t in self.terms]
             self.termNames.sort(key=lambda s: (len(s), s), reverse=True)
 
@@ -23,24 +26,73 @@ class Outputter:
         [self.outputTerm(t) for t in self.terms]
 
     def outputTerm(self, term):
-        # Print the letter headings "## A", "## B" etc
         currentLetter=term['term'][:1]
-        if currentLetter.lower() != self.firstLetter:
-            print('## %s' % currentLetter)
-            print('')
-            self.firstLetter=currentLetter.lower()
+        definition=self.addCrossLinksToTerm(term)
+        serviceList = ''
+        for s in term['services']:
+            serviceList = '%s, %s' % (serviceList, services[s])
 
-        # Print the term and its definition
-        print('### %s' % (term['term'],) )
-        print('')
-        self.outputDefinitionWithCrossLinks(term)
-        print('')
-        if self.outputFormat == 'contributor-guide' and 'usage' in term:
-            print(term['usage'])
+        if self.outputFormat == 'customer-facing':
+            # Print the letter headings "## A", "## B" etc            
+            if currentLetter.lower() != self.firstLetter:
+                print('## %s' % currentLetter)
+                print('')
+                self.firstLetter=currentLetter.lower()
+
+            # Print the term and its definition
+            print('### %s' % (term['term'],) )
+            print('')
+            print(definition)
+            print('')
+            if 'learn-more' in term:
+                print('To learn more, see %s' % term['learn-more'])
+            print ('Applies to: %s' % serviceList[2:])
+
+        if self.outputFormat == 'contributor-guide':
+            # Print the letter headings "## A", "## B" etc
+            if currentLetter.lower() != self.firstLetter:
+                print('## %s' % currentLetter)
+                print('')
+                self.firstLetter=currentLetter.lower()
+
+            # Print the term and its definition
+            print('### %s' % (term['term'],) )
+            print('Applies to: %s' % serviceList[2:])
+            print('')
+            print(definition)
+            print('')
+            if 'usage' in term:
+                self.outputUsage(term['usage'])
+
+            
+        if self.outputFormat == 'dump-all':
+            if 'usage' in term:
+                self.outputUsage(term['usage'])
+            if 'learn-more' in term:
+                print('To learn more, see %s' % term['learn-more'])
+            serviceList = ''
+            for s in term['services']:
+                serviceList = '%s, %s' % (serviceList, services[s])
+            print ('Applies to: %s' % serviceList[2:])
+
+
+    def outputUsage(self, term):
+        if 'casing-rules' in term:
+            print('Casing rules: %s' % (term['casing-rules']))
+            print('')
+        if 'first-mention' in term:
+            print('First mention: %s' % (term['first-mention']))
+            print('')
+        if 'accepted-abbreviation' in term:
+            print('Accepted abbreviation: %s' % (term['accepted-abbreviation']))
+            print('')
+        if 'example-usage' in term:
+            print('Example usage: %s' % (term['example-usage']))
+            print('')
 
     # Add links to other terms in the term list
     # Only link first instance in definition and preserve original case
-    def outputDefinitionWithCrossLinks(self, currentTerm):
+    def addCrossLinksToTerm(self, currentTerm):
         definition = currentTerm['definition']
         for t in self.termNames:
 
@@ -58,10 +110,10 @@ class Outputter:
                     if definition[end] == 's':
                         end = end+1
                     definition = "%s[%s](#%s)%s" % (definition[:start], definition[start:end], slugify(t), definition[end:] )
-        print(definition)
+        return definition
 
 inputFile = sys.argv[1]    # YAML term list file
-outputFormat = sys.argv[2] # customer-facing|contributor-guide
+outputFormat = sys.argv[2] # customer-facing|contributor-guide|dump-all
 
 print('Processing file: %s' % inputFile)
 print('Output format: %s' % outputFormat)
